3b193cdcb13b5d329bdd6043a0f8f83658160311
Manoj <ermanojghimire@gmail.com>
2021-07-19 23:28:40 -0700

    Fake data provider api

--- app.js
+++ app.js
@@ -1,38 +1,33 @@
 const express = require('express');
+const faker = require('faker');
+
 const app = express();
 const PORT = 8081;
 
 
-let mockData = {
-	'page_number': 0,
-	'more_available': false,
-	'spammers': [
-		{
-			'id': 0,
-			'number': '9379093281',
-			'state': 'Ohio',
-			'type': 0 // phone scammer
-		},
-		{
-			'id': 1,
-			'number': '9378932110',
-			'state': 'Ohio',
-			'type': 1 // message scammer
-		},
-		{
-			'id': 2,
-			'number': '5200382390',
-			'state': 'Arizona',
-			'type': 1 // message scammer
-		}
-	]
+
+function getRandomDataSet(count) {
+	let fakeData = {};
+	fakeData['page_number'] = 0;
+	fakeData['more_available'] = false;
+	fakeData['spammers'] = [];
+
+	for (let i = 0; i < count; i++) {
+		fakeData['spammers'].push({
+			'id': i,
+			'number': faker.phone.phoneNumber("!##-!##-####"),
+			'state': faker.address.state(),
+			'type': faker.datatype.number(1)
+		})
+	}
+
+	return fakeData;
 }
 
 
-
 app.get("/latest_spamdb", async (req, res) => {
-	console.log(req.headers);
-	res.send(mockData);
+	let data = getRandomDataSet(1000);
+	res.send(data);
 });
 
 
